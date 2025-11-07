a685bcff9c57bf2dc890d7611a6915d12d743418
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-07 10:36:04 +0500

    Create GetUserDto.cs

---
+++ UserManagmentWebApi.Application/Dtos/Users/GetUserDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentWebApi.Application.Dtos.Users
+{
+    public record GetUserDto(Guid UserId, string FirstName, string LastName, string Email, string Contact, string ImageUrl, string Role, string UserName);
+   
+}
