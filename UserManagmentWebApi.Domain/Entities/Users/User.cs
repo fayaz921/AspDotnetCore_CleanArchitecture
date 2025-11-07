e8de9562b71e411b13163912fe7faace4dc65c8e
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-07 10:36:15 +0500

    Update User.cs

--- UserManagmentWebApi.Domain/Entities/Users/User.cs
+++ UserManagmentWebApi.Domain/Entities/Users/User.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserManagmentWebApi.Domain.Entities.Enums;
 
 namespace UserManagmentWebApi.Domain.Entities.Users
 {
-    internal class User
+    public class User
     {
+        public Guid UserId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+
+        public Role Role { get; set; } = Role.User;
+
+        public string? ImageUrl { get; set; }
+
+        public string Contact { get; set; } = string.Empty;
+
+        public string? OTP { get; set; }
     }
 }
