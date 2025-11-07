d4326b9f0835d746f685d48b80a05519da708025
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-07 10:35:59 +0500

    Create UserRegisterDto.cs

---
+++ UserManagmentWebApi.Application/Dtos/Authentication/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentWebApi.Application.Dtos.Authentication
+{
+    public record UserRegisterDto(string FirstName, string LastName, string Email, string Password, string Contact, string UserName);
+
+}
