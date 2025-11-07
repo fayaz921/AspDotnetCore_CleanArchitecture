93e1732aa90456441eb678cb5e56417fc0589d76
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-07 10:35:55 +0500

    Create UserLoginDto.cs

---
+++ UserManagmentWebApi.Application/Dtos/Authentication/UserLoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentWebApi.Application.Dtos.Authentication
+{
+    public record UserLoginDto(string UserIdentifier, string Password);
+  
+}
